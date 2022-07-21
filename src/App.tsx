5e4d62f169f70d5f20ad151291d58f5d19485fb3
Lukahm <karliuhaomin@hotmail.com>
2022-07-21 18:02:55 +0800

    feat: add login feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+import { LoginScreen } from "screens/login";
 import { ProjectScreen } from "screens/project-list";
+
 function App() {
   return (
     <div className="App">
-      <ProjectScreen></ProjectScreen>
+      <LoginScreen />
+      {/*<ProjectListScreen />*/}
     </div>
   );
 }
